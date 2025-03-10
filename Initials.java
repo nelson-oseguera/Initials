07370cff1e45df176ca1124175a875378bf11023
Nelson Oseguera <nelsonoseguera@outlook.com>
2025-03-10 00:19:57 -0400

    Add files via upload

---
+++ Initials.java
@@ -0,0 +1,34 @@
+package initials;
+
+//Nelson Oseguera [os1148644@email.ccbcmd.edu: ]
+/**
+		* Overview: Write a program that prints out my initials with the letters of my initials
+		* Input: None
+		* Output: Text Display * Variables: None
+		* Plan:
+		* 1. Print out two letters using the same letters of my initials
+		* 2. Program will display my initials made out of my initials
+		*
+		* @version: 1.0 August 30, 2023
+		* @author: Nelson / Oseguera
+*/
+
+public class Initials {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		 System.out.println(" NNNNN       NNNN     OOOOOOOOOO");
+         System.out.println(" NNNNNN      NNNN    OOOOOOOOOOOO");
+         System.out.println(" NNNNNNNN    NNNN    OOOO    OOOO");
+         System.out.println(" NNNNN NNN   NNNN    OOOO    OOOO");
+         System.out.println(" NNNN   NNN  NNNN    OOOO    OOOO");
+         System.out.println(" NNNN    NNN NNNN    OOOO    OOOO");
+         System.out.println(" NNNN     NNNNNNN    OOOO    OOOO");
+         System.out.println(" NNNN      NNNNNN    OOOOOOOOOOOO");
+         System.out.println(" NNNN       NNNNN     OOOOOOOOOO");
+
+
+	}
+
+}
